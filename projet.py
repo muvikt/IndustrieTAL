968213578ce1197c4e12f4a2e7ebc7be74c34e2b
muvikt <muvikt@gmail.com>
2015-12-31 11:44:56 +0100

    First commit

---
+++ projet.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+#title           : 
+#description     : anonymize a sample of enron corpus 
+#authors         : anca boca, victoria musatova 
+#date            : 30/12/15
+#usage           : 
+#python_version  : 2.7
+
+#imports
+import os
+import nltk
+from os import listdir
+from os.path import isfile, join
+from text_processing import *
+
+
+# List all the subdirs of rootd
+# returns a list of string
+def listSubDirs(rootd):
+	""" (str) --> lst of str
+	"""
+	subdirs = list()
+	for subd in os.listdir(rootd):
+		orgd=rootd+"/"+subd
+		if os.path.isdir(orgd):
+		  subdirs.append(orgd)
+	return subdirs
+
+def listFiles(subdirs):
+  files=[]
+  for subd in subdirs:
+    for file in os.listdir(subd):
+      filename=subd+"/"+file
+      files.append(filename)
+  return files
+
+
+def main():
+	#print listSubDirs('scott-s')
+	contentFile = open("mail.txt",'r').read()
+	header_email,body_email = filter_email(contentFile)
+	anonymize_header(header_email)
+ 	#tokenized_body = tokenize(body_email)
+ 	#print tokenized_content
+ 	#tagged_body = tag(tokenized_body)
+ 	#print tagged_content
+ 	#get_EN_body = get_EN(tagged_body)
+
+ 	
+
+
+if __name__ == '__main__':
+	main()
---
+++ text_processing.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+
+#imports
+import os
+import nltk
+import re
+import email
+from os import listdir
+from os.path import isfile, join
+from nltk.data import load
+from nltk.chunk.api import ChunkParserI
+from nltk.chunk.util import (ChunkScore, accuracy, tagstr2tree, conllstr2tree,
+                             conlltags2tree, tree2conlltags, tree2conllstr, tree2conlltags,
+                             ieerstr2tree)
+from nltk.chunk.regexp import RegexpChunkParser, RegexpParser
+from email.parser import HeaderParser
+
+
+
+
+# get the mail header 
+def get_header_email(src):
+	""" (str) --> list of str tuple
+	"""
+	parser = HeaderParser()
+	h = parser.parsestr(src)
+	return h.items()
+
+
+
+
+# get the mail body text
+def get_body_mail(src):
+	""" (str) --> str
+	"""	
+	complet_email = email.message_from_string(src)	
+	body_email = complet_email.get_payload()
+	return body_email
+
+
+
+# split the email intp header and body
+def filter_email(src):
+	""" (src) --> list of str tuple, str
+	"""
+	return get_header_email(src),get_body_mail(src)
+
+
+
+
+#anonymize the email adresses (from & to)
+def anonymize_header(email_header):
+	""" (list of str tuple) --> list of str tuple
+	"""
+	dico_mapping = {}
+	patterns = { 'From' : str, 'To' : str, 'Subject' : str}
+	for item in email_header:
+		string = str(item).replace("(",'').replace(")",'').replace('\'','').replace("\\n\\",'')
+		for p in patterns:
+			if re.match(p, string):
+				string = string.replace(p+",","")
+				patterns[p]=(string.split(","))
+	for p in patterns:
+		if str(p) == 'From' or str(p) == 'To':
+			i =0
+			for item in patterns[p]:
+				i+=1
+				if item not in dico_mapping:
+					dico_mapping[item] = 'adresse'+str(i)+ "@got.com"
+		if str(p) == 'Subject':
+			print patterns[p]
+			
+		
+
+
+#anonymize the email body
+def anonymize_body(email_body):
+	"""
+	"""
+	pass
+
+
+
+
+#return a string without punctuation
+def remove_PUNCT(tokens):
+	""" (str) --> str 
+	"""
+	no_punct = [w for w in tokens if w.isalnum()]
+	return no_punct
+
+
+
+# return tokenized string
+def tokenize(contentFile):
+	""" (str) --> str
+	"""
+	print "Tokenize ..."
+	#remove punctuation
+	tokens = nltk.word_tokenize(contentFile)
+	no_punct_tokens = remove_PUNCT(tokens)
+	return no_punct_tokens
+
+
+
+# return tagged string
+def tag(tokenized_content):
+	""" (str) --> str
+	"""
+	print "Tagging ..."
+	tagged = nltk.pos_tag(tokenized_content)
+	return tagged
+
+
+
+#return a list of tuples(name_entity, str)
+def get_EN(tagged_content):
+	""" (str) --> list of str
+	"""
+	print "Names entity recognition..."
+	lst_EN = list()
+	for item in nltk.ne_chunk(tagged_content, binary=True):
+		lst_EN.append(item)
+	return lst_EN 	
+
+
+
+
